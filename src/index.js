fd967039e0ef8b5df25e552153c44067dbaedf79
Goran Gligorin <goran@gligorin.com>
2017-04-04 21:36:59 +0200

    feat(version): add setVersion to write version number in config.xml

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 'use strict';
 
-module.exports = () => {
+var fs = require('fs');
+var xml2js = require('xml2js');
+var xmlParser = new xml2js.Parser();
+var xmlBuilder = new xml2js.Builder();
 
+module.exports = {
+    setVersion: setVersion
 };
+
+function setVersion(configPath, version, callback) {
+    fs.readFile(configPath, { encoding: 'UTF-8' }, function (readError, configData) {
+        if (readError) {
+            callback(readError);
+        }
+
+        xmlParser.parseString(configData, function (parseError, configXml) {
+            if (parseError) {
+                callback(parseError);
+            }
+
+            configXml.widget.$.version = version;
+            var newConfigData = xmlBuilder.buildObject(configXml);
+            fs.writeFile(configPath, newConfigData, { encoding: 'UTF-8' }, callback);
+        });
+    });
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,40 @@
 var expect = require('chai').expect;
 var cordovaSetVersion = require('../src/index');
+var fs = require('fs-extra');
 
-describe('cordova-set-version', function() {
-    it('should work!', function() {
+var originalPath = './test/config.original.xml';
+var configPath = './test/config.xml';
+var expectedConfigPath = './test/config.expected.xml';
+var fileParams = { encoding: 'UTF-8' };
+
+describe('cordova-set-version', () => {
+    it('should exist', () => {
         expect(cordovaSetVersion).to.exist;
+    });
+
+    describe('setVersion', () => {
+        beforeEach(() => {
+            fs.copySync(originalPath, configPath);
+        });
+
+        it('should exist', () => {
+            expect(cordovaSetVersion).to.have.property('setVersion');
+        });
+
+        it('should be a function', () => {
+            expect(typeof cordovaSetVersion.setVersion).to.equal('function');
+        });
+
+        it('should produce same content as in `config.expected.xml` when passed the argument `1.0.0`', (done) => {
+            cordovaSetVersion.setVersion(configPath, '1.0.0', function (error) {
+                expect(error).to.not.exist;
+
+                var config = fs.readFileSync(configPath, fileParams);
+                var expectedConfig = fs.readFileSync(expectedConfigPath, fileParams);
+                expect(config).to.equal(expectedConfig);
+
+                done();
+            });
+        });
     });
 });
