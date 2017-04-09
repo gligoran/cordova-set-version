cb93a76b3e79ff57edea7a1b6d8cf1c98deaead5
Goran Gligorin <goran@gligorin.com>
2017-04-09 12:24:17 +0200

    test: join config paths into a single object and set cwd to test dir

--- test/index.test.js
+++ test/index.test.js
@@ -4,23 +4,25 @@ const expect = require('chai').expect;
 const cordovaSetVersion = require('../src/index');
 const fs = require('fs-extra');
 
-const goodPath = './test/configs/config.good.xml';
-const badPath = './test/configs/config.bad.xml';
-const missingPath = './test/configs/config.missing.xml';
-const configPath = './test/config.xml';
-const expectedConfigPath = './test/configs/config.expected.xml';
 const fileParams = {encoding: 'UTF-8'};
+const configPaths = {
+    COPY: './config.xml',
+    GOOD: './configs/config.good.xml',
+    MALFORMED: './configs/config.malformed.xml',
+    MISSING: './configs/config.missing.xml',
+    EXPECTED: './configs/config.expected.xml'
+};
 
 describe('cordova-set-version', () => {
+    beforeEach(() => {
+        process.chdir(__dirname);
+    });
+
     it('should exist', () => {
         expect(cordovaSetVersion).to.exist;
     });
 
     describe('setVersion', () => {
-        beforeEach(() => {
-            fs.copySync(goodPath, configPath);
-        });
-
         it('should exist', () => {
             expect(cordovaSetVersion).to.have.property('setVersion');
         });
@@ -30,11 +32,13 @@ describe('cordova-set-version', () => {
         });
 
         it('should produce same content as in `config.expected.xml` when passed the argument `1.0.0`', (done) => {
-            cordovaSetVersion.setVersion(configPath, '1.0.0', function (error) {
+            fs.copySync(configPaths.GOOD, configPaths.COPY);
+
+            cordovaSetVersion.setVersion(configPaths.COPY, '1.0.0', function (error) {
                 expect(error).to.not.exist;
 
-                var config = fs.readFileSync(configPath, fileParams);
-                var expectedConfig = fs.readFileSync(expectedConfigPath, fileParams);
+                var config = fs.readFileSync(configPaths.COPY, fileParams);
+                var expectedConfig = fs.readFileSync(configPaths.EXPECTED, fileParams);
                 expect(config).to.equal(expectedConfig);
 
                 done();
@@ -42,17 +46,17 @@ describe('cordova-set-version', () => {
         });
 
         it('should return an error about missing file', (done) => {
-            cordovaSetVersion.setVersion(missingPath, '1.0.0', function (error) {
+            cordovaSetVersion.setVersion(configPaths.MISSING, '1.0.0', function (error) {
                 expect(error).to.exist;
                 expect(error.message).to.contain('no such file or directory');
-                expect(error.message).to.contain(missingPath);
+                expect(error.message).to.contain(configPaths.MISSING);
 
                 done();
             });
         });
 
         it('should return an error about bad file', (done) => {
-            cordovaSetVersion.setVersion(badPath, '1.0.0', function (error) {
+            cordovaSetVersion.setVersion(configPaths.MALFORMED, '1.0.0', function (error) {
                 expect(error).to.exist;
                 expect(error.message).to.not.contain('no such file or directory');
 
